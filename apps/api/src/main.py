082d8cadabc07c0ff1a38b668c28486bfc6e0b07
Jasper Verbeet <jasperverbeet@gmail.com>
2024-11-22 11:39:21 +0100

    build: fixes root path

--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -1,5 +1,4 @@
 from contextlib import asynccontextmanager
-from urllib.parse import urljoin
 
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
@@ -7,9 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import health, messages, threads
 from src.db.prisma import prisma
-from src.logging import logger
 from src.security.api_token import verify_api_key
-from src.settings import settings
 
 load_dotenv()
 
@@ -21,14 +18,8 @@ async def lifespan(_: FastAPI):
     await prisma.disconnect()
 
 
-logger.info(f"API_ROOT_PATH: {settings.API_ROOT_PATH}")
-
-
 app = FastAPI(
     lifespan=lifespan,
-    root_path=urljoin(settings.API_ROOT_PATH, "api/v1"),
-    docs_url=urljoin(settings.API_ROOT_PATH, "docs"),
-    redoc_url=urljoin(settings.API_ROOT_PATH, "redoc"),
     dependencies=[Depends(verify_api_key)],
 )
 
