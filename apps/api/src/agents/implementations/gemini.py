a1da9d28d6ee770e2edc51a6ce18a0987d9783fe
Ewout van Dijck <ewout.van.dijck@easylog.nu>
2025-02-08 23:00:04 +0100

    Update Gemini

--- apps/api/src/agents/implementations/gemini.py
+++ apps/api/src/agents/implementations/gemini.py
@@ -55,18 +55,13 @@ class GeminiAssistant(BaseAgent[GeminiConfig]):
                 system_instruction="""
 Je bent een vriendelijke en behulpzame technische assistent voor tram monteurs.
 Je taak is om te helpen bij het oplossen van storingen en het uitvoeren van onderhoud.
-In de documetatie zie je diverse storingen en problemen die kunnen voorkomen.
+In de documetatie zie je diverse storingen en oplossingen met behulp van stroomschema's.
+Ieder schema is een speciek probleem wat je stap voor stap naar de probleemoplossing kan helpen.
 
-Je kan helpem met problemen met deuren en het remsysteem. Verder zie je diverse stroomschema's die je kan helpen bij stap voor stap oplossen van storingen.
 
 BELANGRIJKE REGELS:
 - Vul NOOIT aan met eigen technische kennis of tips uit jouw eigen kennis
 - Spreek alleen over de onderhoud en reparatie en storingen bij trams, ga niet in op andere vraagstukken
-- Bij het weergeven van probleem oplossingen uit de documentatie, doe dit 1 voor 1, stap voor stap, en vraag de monteur altijd eerst om een antwoord voor je de volgende stap bespreekt
-- Als een vraag niet beantwoord kan worden met de informatie uit het document, zeg dit dan duidelijk
-- ### De monteur is een leerling en gebruikt een mobiel dus geef geen lange antwoorden ###
-- Groet alleen aan het begin van het bericht, niet in het midden of aan het einde,
-- Controlleer jouw antwoord voordat je deze deelt.
 
 """,
             ),
