6a1e778edfae986b64c59fe3f14f4661307b45e4
Jasper Verbeet <jasperverbeet@gmail.com>
2025-03-13 17:21:19 +0100

    fix: maybe fixes things

--- apps/api/src/jobs/ingest_pdf/ingest_from_upload_file_job.py
+++ apps/api/src/jobs/ingest_pdf/ingest_from_upload_file_job.py
@@ -5,6 +5,7 @@ from prefect import task
 from src.jobs.ingest_pdf.lib.extract_and_process_pdf import extract_and_process_pdf
 from src.lib.prisma import prisma
 from src.lib.supabase import supabase
+from src.logger import logger
 
 
 @task(
@@ -17,72 +18,83 @@ def ingest_from_upload_file_job(
     bucket: str = "knowledge",
     target_path: str = "/",
 ) -> None:
-    processed_pdf = extract_and_process_pdf(file_data)
+    try:
+        prisma.connect()
 
-    if file_name:
-        processed_pdf.file_name = file_name
+        processed_pdf = extract_and_process_pdf(file_data)
 
-    full_pdf_path = os.path.join(target_path, processed_pdf.file_name)
+        if file_name:
+            processed_pdf.file_name = file_name
 
-    supabase.storage.from_(bucket).upload(
-        full_pdf_path,
-        file_data,
-        {
-            "upsert": "true",
-            "content-type": processed_pdf.file_type,
-        },
-    )
-
-    pdf_file_object = prisma.objects.find_first_or_raise(where={"name": full_pdf_path, "bucket_id": bucket})
-
-    processed_pdf_db = prisma.processed_pdfs.upsert(
-        where={
-            "object_id": pdf_file_object.id,
-        },
-        data={
-            "create": {
-                "object_id": pdf_file_object.id,
-                "short_summary": processed_pdf.short_summary,
-                "long_summary": processed_pdf.long_summary,
-                "markdown_content": processed_pdf.markdown_content,
-                "file_type": processed_pdf.file_type,
-            },
-            "update": {
-                "short_summary": processed_pdf.short_summary,
-                "long_summary": processed_pdf.long_summary,
-                "markdown_content": processed_pdf.markdown_content,
-                "file_type": processed_pdf.file_type,
-            },
-        },
-    )
-
-    for image in processed_pdf.images:
-        image_full_path = os.path.join(target_path, image.file_name)
+        full_pdf_path = os.path.join(target_path, processed_pdf.file_name)
 
         supabase.storage.from_(bucket).upload(
-            image_full_path,
-            image.file_data,
+            full_pdf_path,
+            file_data,
             {
                 "upsert": "true",
-                "content-type": image.file_type,
+                "content-type": processed_pdf.file_type,
             },
         )
 
-        image_storage_upload = prisma.objects.find_first_or_raise(where={"name": image_full_path, "bucket_id": bucket})
+        pdf_file_object = prisma.objects.find_first_or_raise(where={"name": full_pdf_path, "bucket_id": bucket})
 
-        prisma.processed_pdf_images.upsert(
-            where={"object_id": image_storage_upload.id},
+        processed_pdf_db = prisma.processed_pdfs.upsert(
+            where={
+                "object_id": pdf_file_object.id,
+            },
             data={
                 "create": {
-                    "processed_pdf_id": processed_pdf_db.id,
-                    "object_id": image_storage_upload.id,
-                    "original_file_name": image.file_name,
-                    "page": image.page,
-                    "summary": image.summary,
+                    "object_id": pdf_file_object.id,
+                    "short_summary": processed_pdf.short_summary,
+                    "long_summary": processed_pdf.long_summary,
+                    "markdown_content": processed_pdf.markdown_content,
+                    "file_type": processed_pdf.file_type,
                 },
                 "update": {
-                    "summary": image.summary,
-                    "page": image.page,
+                    "short_summary": processed_pdf.short_summary,
+                    "long_summary": processed_pdf.long_summary,
+                    "markdown_content": processed_pdf.markdown_content,
+                    "file_type": processed_pdf.file_type,
                 },
             },
         )
+
+        for image in processed_pdf.images:
+            image_full_path = os.path.join(target_path, image.file_name)
+
+            supabase.storage.from_(bucket).upload(
+                image_full_path,
+                image.file_data,
+                {
+                    "upsert": "true",
+                    "content-type": image.file_type,
+                },
+            )
+
+            image_storage_upload = prisma.objects.find_first_or_raise(
+                where={"name": image_full_path, "bucket_id": bucket}
+            )
+
+            prisma.processed_pdf_images.upsert(
+                where={"object_id": image_storage_upload.id},
+                data={
+                    "create": {
+                        "processed_pdf_id": processed_pdf_db.id,
+                        "object_id": image_storage_upload.id,
+                        "original_file_name": image.file_name,
+                        "page": image.page,
+                        "summary": image.summary,
+                    },
+                    "update": {
+                        "summary": image.summary,
+                        "page": image.page,
+                    },
+                },
+            )
+
+    except Exception as e:
+        logger.error(f"Error ingesting from upload file job: {str(e)}")
+        raise e
+    finally:
+        prisma.disconnect()
