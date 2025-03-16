2229bf9c3c96b92fd7447262076f145eb7eab3bb
Jasper Verbeet <jasperverbeet@gmail.com>
2025-03-16 10:50:50 +0100

    chore: changes log level

--- apps/api/src/api/messages.py
+++ apps/api/src/api/messages.py
@@ -95,12 +95,12 @@ async def create_message(
         try:
             async for chunk in forward_message_generator:
                 sse_event = create_sse_event("delta", chunk.model_dump_json())
-                logger.info(f"Sending sse delta event to client: {sse_event}")
+                logger.debug(f"Sending sse delta event to client: {sse_event}")
                 yield sse_event
         except Exception as e:
             logger.exception("Error in SSE stream", exc_info=e)
             sse_event = create_sse_event("error", json.dumps({"detail": str(e)}))
-            logger.info(f"Sending sse error event to client: {sse_event}")
+            logger.warning(f"Sending sse error event to client: {sse_event}")
             yield sse_event
 
     return StreamingResponse(
