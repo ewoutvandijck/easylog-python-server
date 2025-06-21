9219765036762adb9bde176a9382ca692b98ed3a
Jasper Verbeet <jasperverbeet@gmail.com>
2025-06-21 10:01:04 +0200

    chore: adds sentry env vars

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://29d3cd51c24ea2153f619bf8601105ef@o964095.ingest.us.sentry.io/4509535551488000",
+  dsn: 'https://29d3cd51c24ea2153f619bf8601105ef@o964095.ingest.us.sentry.io/4509535551488000',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: false
 });
--- apps/web/src/instrumentation-client.ts
+++ apps/web/src/instrumentation-client.ts
@@ -2,15 +2,13 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://29d3cd51c24ea2153f619bf8601105ef@o964095.ingest.us.sentry.io/4509535551488000",
+  dsn: 'https://29d3cd51c24ea2153f619bf8601105ef@o964095.ingest.us.sentry.io/4509535551488000',
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -24,7 +22,7 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: false
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
